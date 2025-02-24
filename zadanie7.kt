ae3359bd2e266fadacd768948ecf4e7808696db6
Alekber <alekberimamguliev9@gmail.com>
2025-02-24 11:15:29 +0900

    Delete zadanie7.kt

--- zadanie7.kt
+++
@@ -1,7 +0,0 @@
-fun main() {
-    print("Введите два числа: ")
-    val (a, b) = readln().split(" ").map { it.toDouble() }
-
-    println("Большее число: ${maxOf(a, b)}")
-    println("Меньшее число: ${minOf(a, b)}")
-}
