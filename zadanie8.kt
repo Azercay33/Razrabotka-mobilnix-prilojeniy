8320aafe27a6985b0f0a65dc239661c34b6c7ee3
Alekber <alekberimamguliev9@gmail.com>
2025-02-24 11:28:10 +0900

    Delete zadanie8.kt

--- zadanie8.kt
+++
@@ -1,9 +0,0 @@
-fun main() {
-    print("Введите расстояния (в километрах и футах): ")
-    val (km, ft) = readln().split(" ").map { it.toDouble() }
-
-    val metersFromKm = km * 1000
-    val metersFromFt = ft * 0.305
-
-    println(if (metersFromKm < metersFromFt) "Километры меньше" else "Футы меньше")
-}
