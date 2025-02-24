89447d3b98c87c24f06f4bffa3aeac9176ae9fa7
Alekber <alekberimamguliev9@gmail.com>
2025-02-24 11:13:55 +0900

    Delete zadanie11.kt

--- zadanie11.kt
+++
@@ -1,7 +0,0 @@
-fun main() {
-    print("Введите число: ")
-    val num = readln().toInt()
-
-    println(if (num % 2 == 0) "Число четное" else "Число нечетное")
-    println(if (num % 10 == 7) "Число оканчивается на 7" else "Число не оканчивается на 7")
-}
