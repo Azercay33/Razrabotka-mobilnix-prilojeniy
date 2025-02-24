1d667c553153e62f061674b4c9d329e55f06b493
Alekber <alekberimamguliev9@gmail.com>
2025-02-24 11:14:45 +0900

    Delete zadanie3.kt

--- zadanie3.kt
+++
@@ -1,7 +0,0 @@
-fun main() {
-    print("Введите два числа: ")
-    val (a, b) = readln().split(" ").map { it.toInt() }
-
-    val odd = if (a % 2 != 0) a else b
-    println("Нечетное число: $odd")
-}
