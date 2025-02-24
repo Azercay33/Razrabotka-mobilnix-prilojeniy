b1cf6606a9a86ec1b7277c8579d1422637e8a3cf
Alekber <alekberimamguliev9@gmail.com>
2025-02-24 11:12:44 +0900

    Delete alekber7.kt

--- alekber7.kt
+++
@@ -1,7 +0,0 @@
-fun main(){
-    println("Введите три числа: ")
-    val num1 = readLine()!!.toDouble()
-    val num2 = readLine()!!.toDouble()
-    val num3 = readLine()!!.toDouble()
-    println("$num1  $num2  $num3")
-}
